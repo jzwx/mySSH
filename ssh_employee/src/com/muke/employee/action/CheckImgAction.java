fefd06e6aa91bdf8dbb0114f22442f374abd7530
jzwx <2058631403@qq.com>
2017-07-25 17:42:36 +0800

    LIS-0005_wangxin

---
+++ ssh_employee/src/com/muke/employee/action/CheckImgAction.java
@@ -0,0 +1,74 @@
+package com.muke.employee.action;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CheckImgAction extends ActionSupport {
+	public String execute() throws Exception{
+		int width=120;
+		int height=30;
+		BufferedImage bufferedImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+		Graphics graphics=bufferedImage.getGraphics();
+		graphics.setColor(getRandColor(200,250));
+		graphics.fillRect(0, 0, width, height);
+		graphics.setColor(Color.WHITE);
+		graphics.drawRect(0, 0, width-1, height-1);
+		Graphics2D graphics2d=(Graphics2D) graphics;
+		graphics2d.setFont(new Font("Î¢ÈíÑÅºÚ",Font.BOLD,18));
+		String words = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+		Random random=new Random();
+		StringBuffer sb=new StringBuffer();
+		int x=10;
+		for(int i=0;i<4;i++){
+			graphics2d.setColor(new Color(20+random.nextInt(110),20+random.nextInt(110),20+random.nextInt(110)));
+			int jiaodu=random.nextInt(60)-30;
+			double theta=jiaodu*Math.PI/180;
+			int index=random.nextInt(words.length());
+			char c=words.charAt(index);
+			sb.append(c);
+			graphics2d.rotate(theta,x,20);
+			graphics2d.drawString(String.valueOf(c), x, 20);
+			graphics2d.rotate(-theta,x,20);
+			x+=30;
+		}
+		ServletActionContext.getRequest().getSession().setAttribute("checkcode", sb.toString());
+		graphics.setColor(getRandColor(160, 200));
+		int x1;
+		int x2;
+		int y1;
+		int y2;
+		for(int i=0;i<20;i++){
+			x1=random.nextInt(width);
+			x2=random.nextInt(12);
+			y1=random.nextInt(height);
+			y2=random.nextInt(12);
+			graphics.drawLine(x1, y1, x1+x2, y1+y2);
+		}
+		graphics.dispose();
+		ImageIO.write(bufferedImage, "jpg", ServletActionContext.getResponse().getOutputStream());
+		return NONE;
+	}
+	private Color getRandColor(int fc,int bc){
+		Random random=new Random();
+		if(fc>255){
+			fc=255;
+		}
+		if(bc>255){
+			bc=255;
+		}
+		int r=fc+random.nextInt(bc-fc);
+		int g=fc+random.nextInt(bc-fc);
+		int b=fc+random.nextInt(bc-fc);
+		return new Color(r,g,b);
+	}
+}
---
+++ ssh_employee/src/com/muke/employee/action/EmployeeAction.java
@@ -0,0 +1,138 @@
+package com.muke.employee.action;
+/*
+ * 员工管理Action类
+ */
+import java.util.List;
+
+import org.apache.struts2.ServletActionContext;
+import org.omg.CORBA.Request;
+
+import com.muke.employee.domain.Department;
+import com.muke.employee.domain.Employee;
+import com.muke.employee.domain.PageBean;
+import com.muke.employee.service.DepartmentService;
+import com.muke.employee.service.EmployeeService;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class EmployeeAction extends ActionSupport implements ModelDriven<Employee>{
+	//模型驱动使用对象
+	private Employee employee=new Employee();
+	private String checkcode;
+	
+
+	public String getCheckcode() {
+		return checkcode;
+	}
+
+	public void setCheckcode(String checkcode) {
+		this.checkcode = checkcode;
+	}
+
+	@Override
+	public Employee getModel() {
+		// TODO Auto-generated method stub
+		return employee;
+	}
+	//注入业务层的类
+	private EmployeeService employeeService;
+	private DepartmentService departmentService;
+	
+	public void setDepartmentService(DepartmentService departmentService) {
+		this.departmentService = departmentService;
+	}
+
+	public void setEmployeeService(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+	
+	//接收当前的页数
+	private Integer currPage=1;
+
+	public void setCurrPage(Integer currPage) {
+		this.currPage = currPage;
+	}
+
+	public String login() throws Exception{
+		System.out.println("login执行了....");
+		//验证码校验
+		String checkcode1 = (String) ServletActionContext.getRequest().getSession().getAttribute("checkcode");
+		System.out.println("checkcode:"+checkcode1+" this.checkcode:"+this.getCheckcode());
+		//调用业务层类
+		Employee existEmployee=employeeService.login(employee);
+		if(existEmployee==null){
+			//登录失败
+			this.addActionError("用户名或密码错误!");
+			return "input";
+		}else{
+			if(!this.getCheckcode().equalsIgnoreCase(checkcode1)){
+				this.addActionError("验证码错误,请重新输入!");
+				return "input";
+			}
+			//登录成功
+			ActionContext.getContext().getSession().put("existEmployee", existEmployee);//经用户名密码存中
+			return "success";
+		}
+	}
+	
+	/*
+	 * 分页查询员工的执行的方法:
+	 */
+	public String findAll() throws Exception{
+		PageBean<Employee> pageBean=employeeService.findByPage(currPage);
+		ActionContext.getContext().getValueStack().push(pageBean);
+		return "findAll";
+	}
+	
+	/*
+	 * 跳转到添加员工页面执行的方法
+	 */
+	public String saveUI() throws Exception{
+		//查询所有的部门
+		List<Department> list=departmentService.findAll();
+		ActionContext.getContext().getValueStack().set("list", list);
+		return "saveUI";
+	}
+	
+	/*
+	 * 员工保存的方法
+	 */
+	public String save() throws Exception{
+		employeeService.save(employee);
+		return "saveSuccess";
+	}
+	/*
+	 * 编辑员工执行方法
+	 */
+	public String edit() throws Exception{
+		//根据员工ID查询员工
+		employee=employeeService.findById(employee.getEid());
+		List<Department> list=departmentService.findAll();
+		ActionContext.getContext().getValueStack().set("list", list);
+		return "editSuccess";
+	}
+	/*
+	 * 修改员工执行方法
+	 */
+	public String update() throws Exception{
+		employeeService.update(employee);
+		return "updateSuccess";
+	}
+	
+	/*
+	 * 删除员工执行方法
+	 */
+	public String delete() throws Exception{
+		employee=employeeService.findById(employee.getEid());
+		employeeService.delete(employee);
+		return "deleteSuccess";
+	}
+	/*
+	 * 退出登录执行方法
+	 */
+	public String logout() throws Exception{
+		ActionContext.getContext().getSession().remove("existEmployee");
+		return "input";
+	}
+}
